fd3a7e2382e48eb3babc51aabb79edae25508127
AnastasiyaSam <anastasiya.samoilenka@hyperisland.se>
2022-02-03 11:58:16 +0100

    more styling
    styling of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,65 @@
-import React from 'react';
-import styled from 'styled-components';
+import { Search } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
   height: 60px;
-  background-color: black;
-`
+  background-color: red;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid white;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+
 const Navbar = () => {
-  return <Container>navbar</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>En</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>center</Center>
+        <Right>right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
